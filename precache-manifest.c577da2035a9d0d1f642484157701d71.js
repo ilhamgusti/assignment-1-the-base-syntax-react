ebdb0b7c7d2f3d9e5dc6dd777c1915f7b9553df2
ilhamgusti <ilhamgewwe@gmail.com>
2019-08-09 23:25:37 +0700

    Updates

---
+++ precache-manifest.c577da2035a9d0d1f642484157701d71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd0f3e5b9e2a3d906d519dba6a7fdaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "445b0648e2d087706c8f",
+    "url": "/static/css/main.89b8ee42.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "445b0648e2d087706c8f",
+    "url": "/static/js/main.da6cc95e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
